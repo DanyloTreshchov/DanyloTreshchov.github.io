d373695229cb704f44b5d6b382dd0cab7662f0e6
Danylo Treshchov <danylo.treshchov@mycit.ie>
2024-11-23 14:27:16 +0000

    Set up redirect to danigoes.online

--- script.js
+++
@@ -1,9 +0,0 @@
-
-const projecs = document.querySelectorAll('.project');
-
-projecs.forEach(project => {
-    project.addEventListener('click', () => {
-        link = project.querySelector('.project-desc-title').getAttribute('href');
-        window.open(link, '_blank');
-    });
-});
